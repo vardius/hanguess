8f646b8d77b159febe650e448c62e0fa7b095a9d
Rafał Lorenz <vardius@gmail.com>
2026-02-23 01:03:25 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,13 @@
-const CACHE_NAME = "hangul-ascend-v1";
-const urlsToCache = [
-  "index.html",
-  "manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open("hanguess").then(cache=>{
+return cache.addAll(["./","./index.html","./manifest.json"]);
+})
+);
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
 });
